2b650c3ce4e0ad4b111e11f11476ad9f5ac3715b
Mike Summerfeldt <20338451+IT-MikeS@users.noreply.github.com>
2023-05-17 00:21:57 -0400

    chore: no lint in example-app

--- packages/capacitor-plugin/android/src/main/java/com/itmikes/capacitorintents/CapacitorIntents.java
+++ packages/capacitor-plugin/android/src/main/java/com/itmikes/capacitorintents/CapacitorIntents.java
@@ -16,7 +16,6 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
-
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
